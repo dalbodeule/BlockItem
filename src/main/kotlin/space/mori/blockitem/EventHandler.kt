aaafebfd7b6c1622d74f336546ae7c5e20ef1b56
dalbodeule <jioo0224@naver.com>
2020-01-07 02:02:02 +0900

    1.5.1 release

--- src/main/kotlin/space/mori/blockitem/EventHandler.kt
+++ src/main/kotlin/space/mori/blockitem/EventHandler.kt
@@ -1,75 +1,60 @@
 package space.mori.blockitem
 
+import com.codingforcookies.armorequip.ArmorEquipEvent
 import org.bukkit.Material
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
-import org.bukkit.event.inventory.ClickType
-import org.bukkit.event.inventory.InventoryClickEvent
-import org.bukkit.event.inventory.InventoryType
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.inventory.meta.PotionMeta
 import space.mori.blockitem.util.getColored
 
 object EventHandler: Listener {
-    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    internal fun onPlayerClickedInventory(event: InventoryClickEvent) {
-        val slotItem = event.currentItem?.data?.itemType
-        val heldItem = event.cursor?.data?.itemType
 
-        if (slotItem != null && heldItem != null && event.click == ClickType.LEFT) {
-            if (getItemName(heldItem) in Armor) {
-                if (event.slotType == InventoryType.SlotType.ARMOR && getItemName(slotItem) == "air") {
-                    if (!event.whoClicked.hasPermission("blockitem.${getItemName(heldItem)}")) {
-                        event.whoClicked.sendMessage("&4[!]&r You cannot use item ${getItemName(heldItem)}".getColored)
+    @EventHandler(priority = EventPriority.HIGHEST)
+    internal fun onPlayerUseBannedItemWithRightClick(event: PlayerInteractEvent) {
+        val item = event.item?.data?.itemType
+        val potionItem = event.item
+
+        if (item != null && potionItem != null) {
+            if (event.action == Action.RIGHT_CLICK_AIR || event.action == Action.RIGHT_CLICK_BLOCK) {
+                if (Potion.contains(getItemName(item))) {
+                    val potionName = (potionItem.itemMeta as PotionMeta).basePotionData.type
+
+                    if (!event.player.hasPermission("blockitem.potion.$potionName")) {
                         event.isCancelled = true
+                        event.player.sendMessage("&4[!]&r You cannot use potion $potionName".getColored)
+
+                        return
+                    }
+
+                    val customEffects = (potionItem.itemMeta as PotionMeta).customEffects
+
+                    if (customEffects.size > 0) {
+                        customEffects.forEach {
+                            if (!event.player.hasPermission("blockitem.potion.${it.type}")) {
+                                event.isCancelled = true
+                                event.player.sendMessage("&4[!]&r You cannot use potion $potionName".getColored)
+
+                                return
+                            }
+                        }
                     }
                 }
             }
         }
     }
 
-    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    internal fun onPlayerUseBannedItemWithRightClick(event: PlayerInteractEvent) {
-        if (event.hasItem()) {
-            val item = event.item?.data?.itemType
-            val potionItem = event.item
+    @EventHandler
+    internal fun onArmorEquip(event: ArmorEquipEvent) {
+        val item = event.newArmorPiece?.data?.itemType
 
-            if (
-                item != null && potionItem != null &&
-                (event.action == Action.RIGHT_CLICK_AIR || event.action == Action.RIGHT_CLICK_BLOCK)
-            ) {
-                when {
-                    getItemName(item) in Potion -> {
-                        val potionName = (potionItem.itemMeta as PotionMeta).basePotionData.type.name.toLowerCase()
-
-                        if (!event.player.hasPermission("blockitem.potion.$potionName")) {
-                            event.isCancelled = true
-                            event.player.sendMessage("&4[!]&r You cannot use potion $potionName".getColored)
-
-                            return
-                        }
-
-                        val customEffects = (potionItem.itemMeta as PotionMeta).customEffects
-
-                        if (customEffects.size > 0) {
-                            customEffects.forEach {
-                                if (!event.player.hasPermission("blockitem.potion.${it.type.name}")) {
-                                    event.isCancelled = true
-                                    event.player.sendMessage("&4[!]&r You cannot use potion $potionName".getColored)
-
-                                    return
-                                }
-                            }
-                        }
-                    }
-                    getItemName(item) in Armor -> {
-                        if (!event.player.hasPermission("blockitem.armor.${getItemName(item)}")) {
-                            event.isCancelled = true
-                            event.player.sendMessage("&4[!]&r You cannot use item ${getItemName(item)}".getColored)
-                        }
-                    }
+        if (item != null) {
+            if (Armor.contains(getItemName(item))) {
+                if (!event.player.hasPermission("blockitem.armor.${getItemName(item)}")) {
+                    event.isCancelled = true
+                    event.player.sendMessage("&4[!]&r You cannot use item ${getItemName(item)}".getColored)
                 }
             }
         }
